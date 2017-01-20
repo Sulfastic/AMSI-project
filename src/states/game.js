c94428cf671d3cc5fa29d00faea1f553738b563a
Wojciech Grojec <manorips@gmail.com>
2017-01-20 09:50:48 +0100

    Create pattern level monster: waves 1: 1-3 waves 2: 2-4 waves 3: 3-5

--- src/states/game.js
+++ src/states/game.js
@@ -126,8 +126,10 @@ class Game extends Phaser.State {
     }
 
     spawnEnemies(){
+        var maxLvlEnemies = this.game.global.currentWaves+3;
+        var minLvlEnemies = this.game.global.currentWaves+1;
         for(let i=0;i<this.enemySpawnPointsCount;i++){
-            var level = Math.floor(Math.random() * ((this.game.global.currentWaves+2) - 1 + 1)) + 1;
+            var level = Math.floor(Math.random() * (maxLvlEnemies - minLvlEnemies + 1)) + minLvlEnemies;
             this.enemy = new Demon(this.game,this.enemySpawnPoints[i].x,this.enemySpawnPoints[i].y,'demon',4,'Demon',level);//Demon 1st level
             this.enemy.height=64;
             this.enemy.width = 64*147/165;
